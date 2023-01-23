c89cefe2651a0de20c75294acf8f150bb08a5a00
Paraskevi Papagiannoula <115705524+papagiannoula@users.noreply.github.com>
2023-01-23 20:57:27 +0200

    contact changes

--- js/form.js
+++ js/form.js
@@ -1,26 +1,65 @@
 'use strict';
 
-const button = document.getElementById('submit-button');
-const form = document.getElementById('contact-form');
-const name = document.getElementById('name');
-const mail = document.getElementById('mail');
-// const phone = document.getElementById('phone');
+const form = document.forms['contact-form'];
 
+// Handle the submit event
+form.addEventListener('submit', (event) => {
+  // Reference to the form elements
+  const name = form.getElementById['name'];
+  const email = form.getElementById['email'];
+  const veremail = form.getElementById['veremail'];
+  const email_re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/i;
+  const phone = form.getElementById['phone'];
+  const message = form.getElementById['message'];
 
-form.addEventListener ('submit', (e) => {
-    if (name.value.trim() === '' || mail.value.trim() ==='') {
-        e.preventDefault();
-        e.target.elements.name.value = '';
-        e.target.elements.email.value = '';
-        e.target.elements.message.value = '';
-})
+  let isValid = true;
+  let errorMessages = [];
 
-button.addEventListener('click', () => {
-    if (name.value === 'admin' && mail.value === 'admin') {
-      alert('Thank you!');
-    } else {
-      alert('Wrong name or mail!');
-    }
-  });
+  // Name checks
+  if (name.value.trim().length < 3) {
+    // Required
+    console.error('Please enter a valid name');
+    errorMessages.push('Please eneter a valid name');
+    isValid = false;
+  }
 
-  
+  // E-mail checks
+  if (email.value.trim().length === 0) {
+    // Required
+    console.error('E-mail is missing');
+    errorMessages.push('E-mail is missing');
+    isValid = false;
+  } else if (email_re.test(email.value)) {
+    // Valid Value
+    console.error('Please enter a valid email');
+    errorMessages.push('Please enter a valid email');
+    isValid = false;
+  }
+
+  //Verification email test
+  if (email.test(veremail)) {
+    // Valid Value
+    console.error('Please enter a valid email');
+    errorMessages.push('Please enter a valid email');
+    isValid = false;
+  }
+
+  //Phone check
+  if(isNaN(phone) || phone.value.trim().length != 10) {
+    //Required
+    console.error('Please Enter a valid phone');
+    errorMessages.push('Please Enter a valid phone');
+    isValid = false;
+  }
+
+  //Message check
+  if(message.value.trim().length <= 20) {
+    //Required
+    console.error('Please Enter More Than 20 Characters');
+    errorMessages.push('Please Enter More Than 20 Characters');
+    isValid = false;
+  }
+
+  // Prevent submit, if any validation failed
+  if (!isValid) event.preventDefault();
+});
